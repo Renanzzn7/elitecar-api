b159cec26d1ca516242b38f7db7fb047a4d545cf
renanzzn7 <renanvini1st@gmail.com>
2025-11-27 16:00:49 -0300

    Limpa routes.ts: remove comentário duplicado e corrige typo

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ import PedidoVendaController from "./controller/PedidoVendacontroller.js";
 const router = Router(); // Cria um novo roteador
 
 // Cria uma rota no servidor, acessível pelo verbo GET no endpoint /
-// Essa rota apenas exibe uma menssagem para o cliente com o status da API
+// Essa rota apenas exibe uma mensagem para o cliente com o status da API
 router.get("/", (req: Request, res: Response) => {
     res.status(200).json({ mensagem: "Se você está vendo essa mensagem, seu servidor está funcionando." });
 });
@@ -22,7 +22,7 @@ router.get("/api/carros/:idCarro", CarroController.carro);
 // Insere um novo carro no banco
 router.post("/api/carros", CarroController.novo);
 // Atualiza um carro no banco
-router.put("/api/carros/:idCarro", CarroController.atualizar)
+router.put("/api/carros/:idCarro", CarroController.atualizar);
 // Remove um carro no banco
 router.delete("/api/carros/remover/:idCarro", CarroController.remover);
 
@@ -37,7 +37,7 @@ router.get("/api/clientes/:idCliente", ClienteController.cliente);
 router.post("/api/clientes", ClienteController.novo);
 // Atualiza um cliente no banco
 router.put("/api/clientes/:idCliente", ClienteController.atualizar);
-// Remover um cliente do banco
+// Remove um cliente do banco
 router.delete("/api/clientes/remover/:idCliente", ClienteController.remover);
 
 /**
@@ -51,7 +51,7 @@ router.get("/api/pedidos/:idPedido", PedidoVendaController.pedido);
 router.post("/api/pedidos", PedidoVendaController.novo);
 // Atualiza um pedido no banco
 router.put("/api/pedidos/:idPedido", PedidoVendaController.atualizar);
-// Remover um pedido do banco
+// Remove um pedido do banco
 router.delete("/api/pedidos/remover/:idPedido", PedidoVendaController.remover);
 
-export { router }; // Exporta do roteador
+export { router }; // Exporta o roteador
